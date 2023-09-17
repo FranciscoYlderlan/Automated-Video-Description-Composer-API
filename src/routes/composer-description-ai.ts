a04e5a66d8c7bdf4f41aa1926744837382f9c6c8
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-09-17 15:31:59 -0300

    :sparkles: feat: Finished backend routes

--- src/routes/composer-description-ai.ts
+++ src/routes/composer-description-ai.ts
@@ -10,18 +10,38 @@ import { json } from "stream/consumers";
 
 export async function composerDescriptionAIRoute(app : FastifyInstance) {
     
-    app.post('/videos/:id/transcription', async (req) => {
-        const paramsSchema = z.object({
-            id: z.string().uuid() 
-        })
-        
-        const { id } = paramsSchema.parse(req.params)
-
+    app.post('/ai/composer', async (req, rep) => {
+       
         const bodySchema = z.object({
-            prompt: z.string() 
+            id: z.string().uuid(),
+            template: z.string(),
+            temperature: z.number().min(0).max(1).default(0.5),
         })
 
-        const { prompt } = bodySchema.parse(req.body)
-    
+        const { id, template, temperature } = bodySchema.parse(req.body)
+
+        const video = await prisma.video.findUniqueOrThrow({
+            where:{
+                id
+            }
+        })
+
+        if(!video.transcription) {
+            return rep.status(400).send({ error: "O vídeo não gerou transcrição"})
+        }
+
+        const promptMessage =  template.replace('{transcription}', video.transcription)
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo-16k",
+            temperature,
+            messages: [ {
+                role: 'user', content: promptMessage,
+            }]
+        })
+
+        return {
+            response
+        }
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { composerDescriptionAIRoute } from './routes/composer-description-ai'
 
 const PORT = 3333
 
 const app = fastify()
 
-app.register(getAllPromptsRoute)
+app.register(fastifyCors, {
+    origin: '*',
+})
+
 app.register(uploadVideoRoute)
+app.register(getAllPromptsRoute)
 app.register(createTranscriptionRoute)
+app.register(composerDescriptionAIRoute)
+
 
 //serviços de armazenamento
 //amazon S3 aws
