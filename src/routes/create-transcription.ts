d210bda96e0660fc88ac598df3d819e7d5718565
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-09-17 11:55:59 -0300

    :package: chore: Added openai package

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify";
 import { z } from 'zod'
 import { prisma } from '../lib/prisma.ts';
 
+import {createReadStream} from 'node:fs'
+
 export async function createTranscriptionRoute(app : FastifyInstance) {
     
     app.post('/videos/:id/transcription', async (req) => {
@@ -17,9 +19,21 @@ export async function createTranscriptionRoute(app : FastifyInstance) {
 
         const { prompt } = bodySchema.parse(req.body)
 
+        const video = await prisma.video.findUniqueOrThrow({
+          where: {
+            id:id,
+          }
+        })
+
+        const videoPath =  video.path
+        const audioReadStream = createReadStream(videoPath)
+
+        
+
         return {
             id,
             prompt,
+            videoPath,
         }
     })
 }
